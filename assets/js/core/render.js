6a52ca3008f42d363579596f48a6b30cd6948ec9
Rj-codez <rjenot9@gmail.com>
2026-06-05 14:04:36 +0800

    Update render.js

--- assets/js/core/render.js
+++ assets/js/core/render.js
@@ -1,1 +1,9 @@
+import { updateDashboard } from "../ui/dashboardUI.js";
+import { loadAllPackages } from "../ui/packageUI.js";
+import { renderRecentUpdates } from "../ui/historyUI.js";
 
+export function renderApp() {
+    updateDashboard();
+    loadAllPackages();
+    renderRecentUpdates();
+}
