f9d8e52beac1f3c0222cd427092ab9d1250ecc1a
Rj-codez <rjenot9@gmail.com>
2026-06-06 08:51:52 +0800

    Delete assets/js/core/render.js

--- assets/js/core/render.js
+++
@@ -1,9 +0,0 @@
-import { updateDashboard } from "../ui/dashboardUI.js";
-import { loadAllPackages } from "../ui/packageUI.js";
-import { renderRecentUpdates } from "../ui/historyUI.js";
-
-export function renderApp() {
-    updateDashboard();
-    loadAllPackages();
-    renderRecentUpdates();
-}
