68e2a9aa41233b1ab99333ae1c34638e293bdd26
Rj-codez <rjenot9@gmail.com>
2026-06-06 10:02:46 +0800

    Update storage.js

--- assets/js/core/storage.js
+++ assets/js/core/storage.js
@@ -1,39 +1,17 @@
 import { AppState } from "./state.js";
 
-
 const STORAGE_KEY = "logitrack_packages";
 const HISTORY_KEY = "logitrack_history";
 
 export function initState() {
-
     const savedPackages = localStorage.getItem(STORAGE_KEY);
-
-    AppState.packages = savedPackages
-        ? JSON.parse(savedPackages)
-        : {};
-
     const savedHistory = localStorage.getItem(HISTORY_KEY);
 
-    AppState.historyStack.items = savedHistory
-        ? JSON.parse(savedHistory)
-        : [];
-
-    // 🔥 FIX OLD DATA MIGRATION (important)
-    AppState.historyStack.items.forEach(log => {
-
-        if (!log.type) {
-
-            if (log.action === "LOGIN" || log.action === "LOGOUT") {
-                log.type = "AUTH";
-            } else {
-                log.type = "PACKAGE";
-            }
-        }
-    });
+    AppState.packages = savedPackages ? JSON.parse(savedPackages) : {};
+    AppState.historyStack.items = savedHistory ? JSON.parse(savedHistory) : [];
 }
 
 export function savePackages() {
-
     localStorage.setItem(
         STORAGE_KEY,
         JSON.stringify(AppState.packages)
