4d9fc6e0745754cefea49ab1bf716531a31444a9
Rj-codez <rjenot9@gmail.com>
2026-06-06 09:09:30 +0800

    Update dashboardUI.js

--- assets/js/ui/dashboardUI.js
+++ assets/js/ui/dashboardUI.js
@@ -3,21 +3,10 @@ import { AppState } from "../core/state.js";
 
 export function updateDashboard() {
 
-    console.log("=== DASHBOARD UPDATE RUNNING ===");
-    console.log("PACKAGES:", AppState.packages);
-    console.log("COUNT:", Object.keys(AppState.packages).length);
-
     const totalEl = document.getElementById("totalCount");
     const pendingEl = document.getElementById("pendingCount");
     const deliveredEl = document.getElementById("deliveredCount");
     const transitEl = document.getElementById("transitCount");
-
-    console.log("ELEMENTS FOUND:", {
-        totalEl,
-        pendingEl,
-        deliveredEl,
-        transitEl
-    });
 
     if (!totalEl || !pendingEl || !deliveredEl || !transitEl) return;
 
@@ -32,17 +21,10 @@ export function updateDashboard() {
     const inTransit = Object.values(AppState.packages)
         .filter(p => p.status === "In Transit").length;
 
-    console.log({
-        total,
-        pending,
-        delivered,
-        inTransit
-    });
-
     totalEl.textContent = total;
     pendingEl.textContent = pending;
     deliveredEl.textContent = delivered;
-    transitEl.textContent = inTransit;
+    transitEl.textContent = inTransit;  
 }
 
 
